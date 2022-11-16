d56109953c4e767fca801fd60ac33a4a235a16f4
Ethan Newlin <newlin5@gmail.com>
2022-11-16 16:32:35 -0500

    can use csv to populate sequelize db

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,20 @@
 const conn = require('./conn');
-const fs = require('fs');
-const path = require('path');
+const csv = require('csvtojson');
 const Student = require('./Student');
 const Teacher = require('./Teacher');
 
+const csvFilePath =
+	'/Users/ethannewlin/Desktop/fullstack senior phase/sandbox/server/db/csv/students.csv';
+
+// Use this : await csv().fromFile(csvFilePath)
+// to create a json array from a CSV file
+
 const syncAndSeed = async () => {
 	await conn.sync({ force: true });
+	const jsonArray = await csv().fromFile(csvFilePath);
+	jsonArray.forEach(async (student) => {
+		await Student.create({ firstName: student.firstName });
+	});
 	await Promise.all([
 		Student.create({ firstName: 'Ethan' }),
 		Student.create({ firstName: 'Larry' }),
