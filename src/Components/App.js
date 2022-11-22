c7394d108c8bbd9be06ff1e434f923ac39fa51f4
Ethan Newlin <newlin5@gmail.com>
2022-11-22 13:46:47 -0500

    student graph working

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Fileupload from './Fileupload';
+import StudentsGraph from './StudentsGraph';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
@@ -14,9 +15,13 @@ const App = () => {
 				<Grid item>
 					<Link to={'/fileupload'}>Upload Files</Link>
 				</Grid>
+				<Grid item>
+					<Link to={'/studentsgraph'}>Students Graph</Link>
+				</Grid>
 			</Grid>
 			<Routes>
 				<Route path='/fileupload' element={<Fileupload />}></Route>
+				<Route path='/studentsgraph' element={<StudentsGraph />}></Route>
 			</Routes>
 		</Fragment>
 	);
--- src/Components/StudentsGraph.js
+++ src/Components/StudentsGraph.js
@@ -1,33 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { Bar } from 'react-chartjs-2';
-import Box from '@mui/material/Box';
+import React, { useState, useEffect, Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchStudents } from '../store/students';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import {
+	BarChart,
+	Bar,
+	Cell,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	ResponsiveContainer,
+} from 'recharts';
 
 const StudentsGraph = () => {
+	const dispatch = useDispatch();
 	const { students } = useSelector((state) => state);
 	const [data, setData] = useState([]);
-	const options = {
-		title: {
-			display: true,
-			text: 'Students Per Grade',
-			fontSize: 20,
-		},
-		legend: {
-			display: true,
-			position: 'right',
-		},
-	};
+
+	useEffect(() => {
+		dispatch(fetchStudents());
+	}, []);
 
 	useEffect(() => {
 		if (students) {
-			setData(students);
+			const sixth = students.filter(
+				(student) => student.gradelevel === '6'
+			).length;
+			const seventh = students.filter(
+				(student) => student.gradelevel === '7'
+			).length;
+			const eighth = students.filter(
+				(student) => student.gradelevel === '8'
+			).length;
+			const ninth = students.filter(
+				(student) => student.gradelevel === '9'
+			).length;
+			const tenth = students.filter(
+				(student) => student.gradelevel === '10'
+			).length;
+			const eleventh = students.filter(
+				(student) => student.gradelevel === '11'
+			).length;
+			const twelfth = students.filter(
+				(student) => student.gradelevel === '12'
+			).length;
+			setData([
+				{
+					name: 'Grade 6',
+					total: sixth,
+				},
+				{
+					name: 'Grade 7',
+					total: seventh,
+				},
+				{
+					name: 'Grade 8',
+					total: eighth,
+				},
+				{
+					name: 'Grade 9',
+					total: ninth,
+				},
+				{
+					name: 'Grade 10',
+					total: tenth,
+				},
+				{
+					name: 'Grade 11',
+					total: eleventh,
+				},
+				{
+					name: 'Grade 12',
+					total: twelfth,
+				},
+			]);
+			console.log(data);
 		}
-	}, []);
+	}, [students]);
 
 	return (
-		<Box sx={{ width: '100%' }}>
-			<Bar options={options} data={data} />
-		</Box>
+		<Fragment>
+			<br />
+			<Typography variant='h5' align='center'>
+				Total Enrollment by Grade
+			</Typography>
+			<br />
+			<ResponsiveContainer width='100%' height='100%'>
+				<BarChart
+					width={500}
+					height={300}
+					data={data}
+					margin={{
+						top: 5,
+						right: 30,
+						left: 20,
+						bottom: 5,
+					}}
+				>
+					<CartesianGrid strokeDasharray='3 3' />
+					<XAxis dataKey='name' />
+					<YAxis />
+					<Tooltip />
+					<Legend />
+					<Bar dataKey='total' fill='#8884d8' />
+				</BarChart>
+			</ResponsiveContainer>
+		</Fragment>
 	);
 };
 
